fcb81a3565bdd4e591f15242a01e0d1ed47b4766
Ayush Gupta <ayushgupta.ac@gmail.com>
2022-03-12 06:45:22 +0530

    Done Some More Practice(12 March 2022)

--- src/main/java/com/ayush/cms/dao/CustomerDAO.java
+++ src/main/java/com/ayush/cms/dao/CustomerDAO.java
@@ -3,7 +3,6 @@ import com.ayush.cms.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-
 @Repository
 public interface CustomerDAO extends CrudRepository<Customer, Integer> {
     @Override
